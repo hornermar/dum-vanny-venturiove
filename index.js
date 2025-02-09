c3cbcab2bfc8c83ce670fb159cfe76010471ea77
hornermar <hornerova.m@gmail.com>
2025-02-09 22:06:20 +0100

    change slider label

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ let userInteracted = false;
 const rangeSlider = () => {
   const svgImages = document.querySelectorAll(".building-layer");
   const rangeEl = document.querySelector(".range-slider__range");
-  const valueEl = document.querySelector(".range-slider__value");
+  const element = document.querySelector(".element");
+  const elementDescription = document.querySelector(".element-description");
+
+  // const valueEl = document.querySelector(".range-slider__value");
   const rangeWidth = rangeEl.offsetWidth;
   const thumbWidth = 20;
   const max = rangeEl.max;
   const min = rangeEl.min;
 
-  const updateImages = (value) => {
-    valueEl.textContent = layers[value - 1]?.name;
+  const updateElement = (value) => {
+    // valueEl.textContent = layers[value - 1]?.name;
+    element.textContent = layers[value - 1]?.name;
+    elementDescription.textContent = layers[value - 1]?.description || "";
     const left = ((value - min) / (max - min)) * (rangeWidth - thumbWidth);
 
     rangeEl.value = value;
-    valueEl.style.left = `${left}px`;
+    // valueEl.style.left = `${left}px`;
 
     svgImages.forEach((img) => {
       const imgId = parseInt(img.id.replace("layer", ""), 10);
@@ -33,7 +38,7 @@ const rangeSlider = () => {
     userInteracted = true;
     const value = parseInt(rangeEl.value, 10);
 
-    updateImages(value);
+    updateElement(value);
   });
 
   svgImages.forEach((img, index) => {
@@ -41,9 +46,9 @@ const rangeSlider = () => {
 
     setTimeout(() => {
       if (!userInteracted) {
-        updateImages(imgId);
+        updateElement(imgId);
       }
-    }, index * 1000);
+    }, index * 500);
   });
 };
 
--- layers.js
+++ layers.js
@@ -1,50 +1,79 @@
 export const layers = [
   {
     id: 1,
-    name: "Rear facade",
+    name: "Zadní stěna",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc tincidunt ante vitae massa. Vivamus ac leo pretium faucibus.",
   },
   {
     id: 2,
-    name: "Arc",
+    name: "Oblouk",
+    description:
+      "Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est.",
   },
   {
     id: 3,
-    name: "Fake chimney",
+    name: "Falešný komín",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     id: 4,
-    name: "Chimney",
+    name: "Komín",
+    description:
+      "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Integer in sapien. In convallis.",
   },
   {
     id: 5,
-    name: "Chimney",
+    name: "Komín",
+    description:
+      "Curabitur sagittis hendrerit ante. Fusce tellus. Et harum quidem rerum facilis est et expedita distinctio. Fusce nibh.",
   },
   {
     id: 6,
-    name: "Stairs",
+    name: "Schody",
+    description:
+      "Sed convallis magna eu sem. Etiam neque. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero.",
   },
   {
     id: 7,
-    name: "Fake stairs",
+    name: "Falešné schody",
+    description:
+      "Mauris tincidunt sem sed arcu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
   },
   {
     id: 8,
     name: "??",
+    description:
+      "Aliquam erat volutpat. Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede.",
   },
   {
     id: 9,
-    name: "Front facade",
+    name: "Fasáda",
+    description:
+      "In enim a arcu imperdiet malesuada. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna.",
   },
   {
     id: 10,
-    name: "Windows",
+    name: "Okna",
+    description:
+      "Etiam dictum tincidunt diam. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.",
   },
   {
     id: 11,
-    name: "Decoration",
+    name: "Dekorace",
+    description:
+      "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
   },
   {
     id: 12,
-    name: "Decoration",
+    name: "Oblouk",
+    description: "Phasellus rhoncus. Etiam commodo dui eget wisi.",
+  },
+  {
+    id: 13,
+    name: "Dekorace",
+    description:
+      "Nullam at arcu a est sollicitudin euismod. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam.",
   },
 ];
